5b99c06518aa0a1be863d9dbc74116c427c3b9ea
Matt List <mattylist@gmail.com>
2019-12-02 23:11:42 +1300

    Basic login screen built

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gradient_text/gradient_text.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,15 +11,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
+  MyHomePage({Key key,}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -29,19 +28,119 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Text(
-              'Backyard Botany',
-              style: TextStyle(
-                fontSize: 30.0,
-                fontWeight: FontWeight.bold
-              ),
-            )
-          ],
-        ),
-      )
+      backgroundColor: Colors.white,
+        body: Padding(
+          padding: const EdgeInsets.only(top: 50.0),
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                    child: Image(
+                      image: AssetImage('assets/images/illustration_1.png'),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: Center(
+                    child: GradientText(
+                      'Backyard Botany',
+                      style: TextStyle(
+                        fontSize: 40.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      gradient: LinearGradient(
+                        colors: <Color>[
+                          Color(0xFF01AA94),
+                          Color(0xFF2CB47A),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 25.0
+                    ),
+                    decoration: new InputDecoration(
+                      hintText: "Username",
+                      hintStyle: TextStyle(
+                        fontSize: 25.0,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF01AA94), width: 0.4),
+                        borderRadius: BorderRadius.circular(30.0)
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF01AA94), width: 0.4),
+                        borderRadius: BorderRadius.circular(30.0)
+                      ),
+                      //border: ...
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 25.0
+                    ),
+                    decoration: new InputDecoration(
+                      hintText: "Password",
+                      hintStyle: TextStyle(
+                        fontSize: 25.0,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF01AA94), width: 0.4),
+                        borderRadius: BorderRadius.circular(30.0)
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Color(0xFF01AA94), width: 0.4),
+                        borderRadius: BorderRadius.circular(30.0)
+                      ),
+                      //border: ...
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 24.0),
+                  child: RaisedButton(
+                    onPressed: () {},
+                    textColor: Colors.white,
+                    padding: const EdgeInsets.all(0.0),
+                    //shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(0.0)),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 50.0,
+                      decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: <Color>[
+                            Color(0xFF01AA94),
+                            Color(0xFF94C73B),
+                          ],
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(00.0))
+                      ),
+                      child: Center(
+                        child: Text(
+                            'Login',
+                            style: TextStyle(fontSize: 28)
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
     );
   }
 }
